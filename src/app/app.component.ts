92c8b7809e67dfee8a41807168a996ced8ca83b1
Connor <connorzimmerman799@yahoo.com>
2018-09-19 21:41:06 -0700

    project update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,39 @@ import 'rxjs/add/operator/catch';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  imageObject = {};
+  imageObject0 = {};
+  imageObject1 = {};
+  imageObject2 = {};
+  imageObject3 = {};
+  imageObject4 = {};
+  imageObject5 = {};
+  imageObject6 = {};
+  imageObject7 = {};
+  imageObject8 = {};
+  coordinateArr = [];
   errorMsgBool = false;
   errorMsg = "";
   renderImg = false;
+  parsedLatInt = 0;
+  parsedLongInt = 0;
+  finalCoords = "";
   constructor(private _dataService: DataService) {
   }
-  RetrieveImage(coordinates) {
-    this._dataService.RetrieveImage(coordinates.trim()).subscribe(data => {
-      this.imageObject = data;
-      this.errorMsg = "";
-      this.renderImg = true;
-    },
-      error => {
-        this.errorMsgBool = true;
-        this.errorMsg = "Sorry, it appears the coordinates you entered could not render an image! Please try again. Examples: 100.75,1.5 or 78,9";
-        this.renderImg = false;
-      });;
+  RetrieveImages(coordinates) {
+
+    this.coordinateArr = this._dataService.CalculateAllCoordinates(coordinates.trim());
+
+    for (let i = 0; i < this.coordinateArr.length; i++) {
+      this._dataService.RetrieveImage(this.coordinateArr[i]).subscribe(data => {
+        this["imageObject" + i] = data;
+        this.errorMsg = "";
+        this.renderImg = true;
+      },
+        error => {
+          this.errorMsgBool = true;
+          this.errorMsg = "Sorry, it appears the coordinates you entered could not render an image! Please try again. Examples: 100.75,1.5 or 78,9";
+          this.renderImg = false;
+        });;
+    }
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,6 +9,117 @@ export class DataService {
     return this.http.get("https://api.nasa.gov/planetary/earth/imagery?lon=" + this.ParseLong(coordinates) + "&lat=" + this.ParseLat(coordinates) + "&api_key=AQOHiLBfEMzpZOFKLs73L3LGGJjfqFu3JOZmZpwr");
   }
 
+  CalculateAllCoordinates(coordinates) {
+    let coordinatesArr = [];
+    for (let i = 0; i < 9; i++) {
+      let parsedLongInt = 0;
+      let parsedLatInt = 0;
+      let finalCoords = "";
+      coordinatesArr.push(this.CalculateCoordinate(i, coordinates, parsedLongInt, parsedLatInt, finalCoords));
+    }
+    return coordinatesArr;
+  }
+
+  CalculateCoordinate(counter, coordinates, parsedLongInt, parsedLatInt, finalCoords) {
+    switch (counter) {
+      // Top 1
+      case 0:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLongInt -= 0.025;
+      parsedLatInt += 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+
+      // Top 2
+      case 1:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLatInt += 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+
+      // Top 3
+      case 2:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLongInt += 0.025;
+      parsedLatInt += 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+
+      // Center 1
+      case 3:
+        parsedLatInt = parseInt(this.ParseLat(coordinates));
+        parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+        parsedLongInt -= 0.025;
+
+        finalCoords = parsedLongInt.toString() + ",";
+        finalCoords += parsedLatInt.toString();
+        return finalCoords;
+
+      // Center 2
+      case 4:
+        return coordinates;
+
+      // Center 3
+      case 5:
+        parsedLatInt = parseInt(this.ParseLat(coordinates));
+        parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+        parsedLongInt += 0.025;
+
+        finalCoords = parsedLongInt.toString() + ",";
+        finalCoords += parsedLatInt.toString();
+        return finalCoords;
+
+      // Bottom 1
+      case 6:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLongInt -= 0.025;
+      parsedLatInt -= 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+
+      // Bottom 2
+      case 7:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLatInt -= 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+
+      // Bottom 3
+      case 8:
+      parsedLatInt = parseInt(this.ParseLat(coordinates));
+      parsedLongInt = parseInt(this.ParseLong(coordinates));
+
+      parsedLongInt += 0.025;
+      parsedLatInt -= 0.025;
+
+      finalCoords = parsedLongInt.toString() + ",";
+      finalCoords += parsedLatInt.toString();
+      return finalCoords;
+    }
+  }
+
   ParseLong(coordinates) {
     let x = "";
     for (let digit of coordinates) {
